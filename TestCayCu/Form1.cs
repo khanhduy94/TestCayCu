ff4c5c6b32c2a8b6de5b1031b1d6ea997efeb5b9
Khanh Duy <lukhach1990@gmail.com>
2015-11-16 00:44:50 +0700

    SeverSide
    Truyen File giua 2 mang internet

---
+++ TestCayCu/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace TestCayCu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public TcpListener tcpListener;
+        public string fileName;
+        public string filePath;
+        public IPAddress LocalAddress;
+        public const int PORT_RECEIVE = 1025;
+        public const int PORT_SEND    = 1026;
+        public List<string> list_Files;
+        //public string[] files = new string[10];
+        public const int BUFFER = 1024;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void SendFile(string fileName)
+        {
+            
+        }       
+        public void listenClient()
+        {
+            LocalAddress = IPAddress.Parse("127.0.0.1");
+            tcpListener = new TcpListener(LocalAddress,PORT_RECEIVE);
+            tcpListener.Start();
+             
+
+            while(true)
+            {
+                Socket socket = tcpListener.AcceptSocket();
+                NetworkStream netStream = new NetworkStream(socket);
+                StreamReader sReader = new StreamReader(netStream);
+                fileName = sReader.ReadLine();
+                if (list_Files.Contains(fileName)&&fileName=="Quit") break;
+                list_Files.Add(fileName);
+                filePath= "E:/" + fileName;
+                FileStream InputFile = new FileStream(filePath, FileMode.CreateNew, FileAccess.Write, FileShare.None);
+                BinaryWriter BinaryStream = new BinaryWriter(InputFile); 
+                byte[] data = new byte[BUFFER];
+                do
+                {
+                    socket.Receive(data);
+                    BinaryStream.Write(data);
+
+                } while (data.Length != 0);
+                BinaryStream.Close();
+                InputFile.Close();
+
+            }
+
+        }
+       
+    }
+}
